95347a5a7e3552a2417292e32fdeeb98c7659b32
LaithSarraf <laith.sarraf@gmail.com>
2021-06-10 16:56:06 +0300

    checked problem3

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -28,18 +28,19 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-let times = a*b
-return [times,`The product of ${a} and ${b} is ${times}.`];
-}
+// function multiply(a, b) { //eslint-disable-line
+// let times = a*b
+// return [times,`The product of ${a} and ${b} is ${times}.`];
+// }
 
-// Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// // Here is the test for multiply(); uncomment it to run it
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 3
+
 Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
@@ -49,13 +50,18 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
+
+
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+let sum2 = a + b + c
+let times = a * b * c
+return [sum2, times, `${a} and ${b} and ${c} sum to ${sum2}.`, `The product of ${a} and ${b} and ${c} is ${times}.`]
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
