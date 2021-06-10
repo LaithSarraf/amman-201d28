4e884e5d0b9ecae95c84ac7a05988eae6d382dad
LaithSarraf <laith.sarraf@gmail.com>
2021-06-10 16:35:31 +0300

    Checked problem 2

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -30,7 +30,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
 let times = a*b
-return [times,'The product of 5 and 9 is 45.'];
+return [times,`The product of ${a} and ${b} is ${times}.`];
 }
 
 // Here is the test for multiply(); uncomment it to run it
